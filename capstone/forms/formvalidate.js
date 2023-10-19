06ba1dcac0f76af3a8f2d29d3947dbe075a44051
Adelahpaul <pauladelah1@gmail.com>
2023-10-20 00:35:02 +0100

    good

--- capstone/forms/formvalidate.js
+++ capstone/forms/formvalidate.js
@@ -1,38 +1,42 @@
-function validatate_form (){
-const validateEmail = (email) => {
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+    const submitForm = document.getElementById('form-msg');
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const  subject = document.getElementById('subject')
+    const textarea = document.getElementById('Message')
 
-    return email.match(
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-  };
-  
-  const validate = () => {
-    const $result = $('#result');
-    const email = $('#email').val();
-    $result.text('');
-  
-    if(validateEmail(email)){
-      $result.text(email + ' is valid.');
-      $result.css('color', 'green');
-    } else{
-      $result.text(email + ' is invalid.');
-      $result.css('color', 'red');
+    submitForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const name = nameInput.value.trim();
+        const email = emailInput.value.trim();
+        const sub = suject.value.trim();
+        const msg = textarea.value.trim()
+
+        if (validateForm(name, email, sub, msg)) {
+            displaySuccessMessage();
+        } else {
+            displayErrorMessage();
+        }
+    });
+
+    function validateForm(name, email, sub, msg) {
+        return !(name === '' || email === '' || sub==='' || msg ==='');
     }
-    return false;
-  }
-  
-  $('#email').on('input', validate);
-//
-function myFunction() {
-  // Get the value of the input field with id="numb"
-  let x = document.getElementById("name").value;
-  // If x is Not a Number or less than one or greater than 10
-  let text;
-  if (isNaN(x) || x == [a-z] || x == [A-Z] ) {
-    text = "Input not valid";
-  } else {
-    text = "Input OK";
-  }
-  document.getElementById("demo").innerHTML = text;
-}
-}
+
+    function displaySuccessMessage() {
+        const successMessage = document.createElement('p');
+        successMessage.textContent = '';
+        successMessage.style.color = 'green';
+        submitForm.appendChild(successMessage);
+    }
+
+    function displayErrorMessage() {
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = 'Please fill in all required fields.';
+        errorMessage.style.color = 'red';
+        submitForm.appendChild(errorMessage);
+    }
+});
+</script>
